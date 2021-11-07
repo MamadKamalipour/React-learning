03a1335e225c9a535c10910fb3dbb80a75e812e6
Mohammad Kamalipour <mohammad.kp8093@gmail.com>
2021-11-07 11:53:04 +0330

    adssad

--- shopping-card/src/components/ProductDetail/ProductDetail.js
+++ shopping-card/src/components/ProductDetail/ProductDetail.js
@@ -1,28 +1,41 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 // context
 import { ProductContext } from "../../context/ProductContextProvider";
 
 const ProductDetail = (props) => {
   const id = props.match.params.id;
+  const [product, setProduct] = useState("");
+
   const data = useContext(ProductContext);
-  const product = data[id - 1];
-  const { image, title, description, price, category } = product;
-  return data ? (
-  
-  <div>
-      <img src={image} alt={`${title} Pic`} />
-      <div>
-        <h3>{title}</h3>
-        <p>{description}</p>
-        <p><span>Category: </span>{category}</p>
+  useEffect(() => {
+    if (data.length > 0) {
+      setProduct(data[id - 1]);
+    }
+  }, [data]);
+
+  const {  title, description, price, category } = product;
+
+  return product ? (
+    <div>
+        <img src={product.image} alt={`${title} Pic`} />
         <div>
+          <h3>{title}</h3>
+          <p>{description}</p>
+          <p>
+            <span>Category: </span>
+            {category}
+          </p>
+          <div>
             <span>{price}$</span>
             <Link to="/products">Go Back Shopping</Link>
+          </div>
         </div>
       </div>
-    </div>
-  ):<h1>Loading...</h1> ;
+
+  ) : (
+    <h1>Loading...</h1>
+  );
 };
 
 export default ProductDetail;
