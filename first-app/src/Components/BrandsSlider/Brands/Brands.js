7037044b819faf28fba86d79b835846638374bd3
Mohammad Kamalipour <mohammad.kp8093@gmail.com>
2021-11-21 15:24:50 +0330

    removed bullshit projects

--- first-app/src/Components/BrandsSlider/Brands/Brands.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import styles from './Brands.module.css'
-export default function Brands(props) {
-    return (
-        
-            <img className={styles.brandImage} src={props.img} alt={props.alt} />
-        
-    )
-}
